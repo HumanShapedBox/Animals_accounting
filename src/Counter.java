7aa71b741df99d33dd79d391f697142742feaa3c
Ksenia <stashksenia@gmail.com>
2023-09-17 08:52:52 +0300

    counter

---
+++ src/Counter.java
@@ -0,0 +1,12 @@
+public class Counter {
+
+    int animalCounter;
+
+    public Counter(){
+        this.animalCounter = 0;
+    }
+
+    public void addAnimal(){
+        this.animalCounter += 1;
+    }
+}
