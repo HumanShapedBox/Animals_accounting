112903bae2193ef30f34cac3da38f7be4fc23638
Ksenia <stashksenia@gmail.com>
2023-09-17 07:59:21 +0300

    random forever

--- src/PackAnimal.java
+++ src/PackAnimal.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PackAnimal extends Animal{
+
+    int strength;
     public PackAnimal(String name, String type, ArrayList<String> commands) {
         super(name, type, commands);
+        this.strength = new Random().nextInt(10);
     }
 }
--- src/Pet.java
+++ src/Pet.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class Pet extends Animal{
+
+    int cuteness;
     public Pet(String name, String type, ArrayList<String> commands) {
         super(name, type, commands);
+        this.cuteness = new Random().nextInt(10);
     }
 }
